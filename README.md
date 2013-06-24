c442af37ecce3453c5ff0180fe4c7c6dcb8a5d50
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-24 15:07:50 -0700

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# npmd-git-resolve
+
+get the package.json of a dependency specified as a git:// url.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,89 @@
+var mkdirp = require('mkdirp')
+var http = require('https')
+var fs = require('fs')
+var crypto = require('crypto')
+var tar = require('tar')
+var os = require('os')
+var zlib = require('zlib')
+var fstream = require('fstream')
+var path = require('path')
+
+function toGithubDownload (repo) {
+  //git://github.com/substack/sockjs-client.git#browserify-npm
+  //https://github.com/isaacs/readable-stream/archive/master.tar.gz
+  //https://codeload.github.com/substack/node-browser-resolve/tar.gz/dir-replace
+  if(/^http/.test(repo)) return repo
+
+  var m = /^git:\/\/github.com\/([^#]+?).git(?:#(.*))?$/.exec(repo)
+
+  if(m) return 'https://codeload.github.com/' + m[1] + '/tar.gz/' + m[2]
+  return null
+}
+
+var resolve = module.exports = function (url, cb) {
+  var hash, n = 2
+  var tmp = path.join(os.tmpdir(), ''+ Date.now() + Math.random())
+  var tmpFile = path.join(tmp, 'package.tgz')
+  var tmpDir = path.join(tmp, 'package')
+  var _url = toGithubDownload(url)
+  mkdirp(tmp, function () {
+    http.get(_url, function (res) {
+      res
+        .pipe(zlib.createGunzip())
+        .pipe(tar.Extract({path: tmp}))
+        .on('close', next)
+
+      var hash = crypto.createHash('sha')
+      res.on('data', function (b) {
+        hash.update(b)
+      })
+      .on('end', next)
+
+        function next () {
+          if(--n) return
+          var h = hash.digest('hex')
+
+          fs.readdir(tmp, function (err, ls) {
+            var source  = path.join(tmp, ls[0])
+            var dest    = path.join(tmp, 'package')
+            var tarball = path.join(tmp, 'package.tgz')
+
+            fs.rename(source, dest, function (err) {
+              var n = 2, pkg
+              fstream.Reader({path: dest, type: 'Directory'})
+                .pipe(tar.Pack({path: dest}))
+                .pipe(zlib.createGzip())
+                .pipe(fs.createWriteStream(tarball))
+                .on('finish', next)
+              
+              fs.readFile(path.join(tmp, 'package', 'package.json'), 'utf8',
+                function (err, file) {
+                  try { pkg = JSON.parse(file) }
+                  catch (err) { return cb(err) }
+                  next()
+                })
+
+              function next () {
+                if(--n) return
+                var cache = path.join(process.env.HOME, '.npm', pkg.name, h)
+                fs.rename(tmp, cache, function (err) {
+                  pkg.hash = h
+                  pkg.from = url
+                  cb(cache, pkg)
+                })
+              }
+            })
+          })
+        }
+    })
+  })
+}
+
+
+if(!module.parent) {
+  var url = "https://codeload.github.com/substack/node-browser-resolve/tar.gz/dir-replace"
+  url = 'git://github.com/substack/sockjs-client.git#browserify-npm'
+  resolve(url, console.error)
+}
+
+
