57aa8094dcadfd3828377b52d9fb58c314c7c6af
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-26 12:24:30 -0700

    tests

---
+++ test/download.js
@@ -0,0 +1,19 @@
+
+var test = require('tape')
+
+var resolve = require('../')
+
+function t (name, url) {
+  test(name + ' <- ' + url, function (t) {
+    resolve(url, function (err, pkg) {
+      console.error(err, pkg)
+      t.equal(pkg.name, name)
+      t.end()
+    })
+  })
+}
+
+
+t('readable-stream', 'git://github.com/isaacs/readable-stream.git')
+t('sockjs-client', 'git://github.com/substack/sockjs-client.git#browserify-npm')
+t('readable-stream', 'https://github.com/isaacs/readable-stream/archive/master.tar.gz')
